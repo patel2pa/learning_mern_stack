b761f97250bb55fb9a9b13331ed33345f72e2469
patel2pa <parthpp1996@gmail.com>
2022-07-08 22:31:41 -0400

    added some validation on user post requests

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator/check'); 
 
+const User = require('../../models/User');
 
 // @route GET api/users
 // @desc | Test route
@@ -11,14 +12,29 @@ router.post('/', [
 check('name', 'Name is required').not().isEmpty(), 
 check('email', 'please include valid email').isEmail(), 
 check('password', 'please enter a password with 6 or more characters').isLength({min: 6})
-],(req, res) => 
+],
+async (req, res) => 
 {
 const errors = validationResult(req);
 if(!errors.isEmpty()){
     return res.status(400).json({errors: errors.array()});
 }
-console.log(req.body)
-res.send('User route');
+
+
+const { name, email, password} = req.body;
+
+try{
+    let user = await User.findOne({email});
+
+    if(user){
+        res.status(400).json({errors: [{msg: 'User already exist'}]});
+    }
+
+    console.log(req.body)
+    res.send('User route');
+}catch(err){}
+    console.error(err.massage);
+    res.status(500).send('server error');
 })
 
 module.exports = router;
